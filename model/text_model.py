35f8b793260e9d0de56a11017bc2cdbd16cef9cf
Gajanan Shenvi <122141986+GajananShenvi@users.noreply.github.com>
2026-01-16 17:41:37 +0530

    Add files via upload

--- model/text_model.py
+++ model/text_model.py
@@ -4,13 +4,15 @@ import torch.nn as nn
 class TextClassifier(nn.Module):
     def __init__(self, input_dim, num_classes):
         super().__init__()
-        self.fc1 = nn.Linear(input_dim, 256)
+        self.fc1 = nn.Linear(input_dim, 128)
+        self.bn1 = nn.BatchNorm1d(128)
         self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(0.3)
-        self.fc2 = nn.Linear(256, num_classes)
+        self.dropout = nn.Dropout(0.2)
+        self.fc2 = nn.Linear(128, num_classes)
 
     def forward(self, x):
         x = self.fc1(x)
+        x = self.bn1(x)
         x = self.relu(x)
         x = self.dropout(x)
         x = self.fc2(x)
